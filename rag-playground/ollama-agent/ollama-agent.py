cdf64c8425e1ee55e1d136ff1a4f670895c729d0
Teik <teik.lee@capgemini.com>
2024-02-29 09:08:28 -0500

    json agent getting there...

--- rag-playground/ollama-agent/ollama-agent.py
+++ rag-playground/ollama-agent/ollama-agent.py
@@ -1,7 +1,7 @@
 import json
 import os
 from pprint import pprint
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI
 from langchain_community.llms import Ollama
 from langchain.agents import AgentExecutor
@@ -13,6 +13,11 @@ from langchain.prompts import PromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain.agents import tool
 from langchain.agents import Tool
+from langchain.agents.format_scratchpad import format_log_to_messages
+from langchain.agents.output_parsers import (
+    ReActJsonSingleInputOutputParser,
+)
+from langchain.tools.render import render_text_description_and_args
 
 from tools.turn_lights_on import TurnLightsOnTool
 from tools.turn_lights_off import TurnLightsOffTool
@@ -30,6 +35,7 @@ llm = ChatOpenAI(openai_api_key=apiKey, model_name="gpt-3.5-turbo", temperature=
 # Ollama
 # llm = Ollama(base_url='http://localhost:11434', model='llama2')
 
+
 class CustomTool:
     def __init__(self):
         prompt = PromptTemplate(input_variables=["query"], template="{query}")
@@ -37,59 +43,89 @@ class CustomTool:
         # when giving tools to LLM, we must pass as list of tools
         tools = [TurnLightsOffTool(), TurnLightsOnTool(), GetSensorDataTool()]
 
-        template = """
-            You are very helpful assistant that will answer the following questions as best you can. 
-            
-            You have access to the following tools.
-            {tools}
+        tool_list = (
+            render_text_description_and_args(tools)
+            .replace("{", "{{")
+            .replace("}", "}}")
+        )
+        tool_names = [t.name for t in tools]
 
-            You should only use the tools only if the question is specifically about lights, sensors and arithmetic.
+        template = f"""Answer the following questions as best you can.
+            You can answer directly if the user is greeting you or similar.
+            Otherise, you have access to the following tools:
 
-            Use the following format:
+            {tool_list}
+
+            The way you use the tools is by specifying a json blob.
+            Specifically, this json should have a `action` key (with the name of the tool to use)
+            and a `action_input` key (with the input to the tool going here).
+            The only values that should be in the "action" field are: [{tool_names}]
+            The $JSON_BLOB should only contain a SINGLE action, 
+            do NOT return a list of multiple actions.
+            Here is an example of a valid $JSON_BLOB:
+            ```
+            {{{{
+                "action": $TOOL_NAME,
+                "action_input": $INPUT
+            }}}}
+            ```
+            The $JSON_BLOB must always be enclosed with triple backticks!
+
+            ALWAYS use the following format:
             Question: the input question you must answer
             Thought: you should always think about what to do
-            Action: the action to take, should be one of [{tool_names}]
-            Action Input: the input to the action
-            Observation: the result of the action
-            ... (this Thought/Action/Action Input/Observation can repeat N times)
-            Thought: I now know the final answer.
+            Action:```
+            $JSON_BLOB
+            ```
+            Observation: the result of the action... 
+            (this Thought/Action/Observation can repeat N times)
+            Thought: I now know the final answer
             Final Answer: the final answer to the original input question
-        
-            Question: Why is the sky blue?
-            Thought: This question cannot be appropriately answered by any of the Tools.
-            Observable: The question cannot be answered.
-            Final Answer: Sorry, I cannot answer that question.
 
-            Begin!
-            
-            Question: {input}
-            Thought:{agent_scratchpad}
+            Begin! Reminder to always use the exact characters `Final Answer` when responding.'
         """
-        prompt = ChatPromptTemplate.from_template(template=template)
-        prompt = prompt.partial(
-            tools=render_text_description(list(tools)),
-            tool_names=", ".join([t.name for t in tools]),
+        # prompt = ChatPromptTemplate.from_template(template=template)
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                (
+                    "user",
+                    template,
+                ),
+                ("user", "{input}"),
+                MessagesPlaceholder(variable_name="agent_scratchpad"),
+            ]
         )
-
-        pprint(prompt.input_variables)
-        pprint(prompt.partial_variables)
-
         llm_with_stop = llm.bind(stop=["\nObservation"])
-        zero_shot_agent_chain = (
-            RunnablePassthrough.assign(
-                agent_scratchpad=lambda x: format_log_to_str(x["intermediate_steps"]),
-            )
+        agent = (
+            {
+                "input": lambda x: x["input"],
+                "agent_scratchpad": lambda x: format_log_to_messages(x["intermediate_steps"]),
+            }
             | prompt
             | llm_with_stop
-            | ReActSingleInputOutputParser()
+            | ReActJsonSingleInputOutputParser()
         )
 
-        self.agent_executor = AgentExecutor(agent=zero_shot_agent_chain, tools=tools, verbose=True, handle_parsing_errors=True)
+        # zero_shot_agent_chain = (
+        #     RunnablePassthrough.assign(
+        #         agent_scratchpad=lambda x: format_log_to_str(x["intermediate_steps"]),
+        #     )
+        #     | prompt
+        #     | llm_with_stop
+        #     | ReActSingleInputOutputParser()
+        # )
+
+        self.agent_executor = AgentExecutor(
+            agent=agent,
+            tools=tools,
+            verbose=True,
+            handle_parsing_errors=True,
+        )
 
     def _handle_error(self, error) -> str:
         print("FUBAR")
         return str(error)
-    
+
     def run(self):
         while True:
             user_input = input(">> ")
@@ -102,6 +138,7 @@ class CustomTool:
                 response = self.agent_executor.invoke({"input": user_input})
                 # print(response['output'])
                 print(response)
+
 
 if __name__ == "__main__":
     customTool = CustomTool()
